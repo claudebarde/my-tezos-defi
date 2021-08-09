f32ccd58e8fb5de8ec0bace1087cc2ff1d828609
Claude Barde <lotolosenc.oc@gmail.com>
2021-08-09 15:08:44 +0100

    Updated version number in footer

--- src/lib/Footer/Footer.svelte
+++ src/lib/Footer/Footer.svelte
@@ -59,6 +59,6 @@
       href="https://github.com/claudebarde/my-tezos-defi"
       target="_blank"
       rel="noopener noreferrer nofollow">Github</a
-    > - v2.7.0
+    > - v3.1.0
   </div>
 </footer>
